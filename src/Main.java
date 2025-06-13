a4ddff22a908062bb26d5391bf095d12f9b1ffc7
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-13 10:30:56 +0900

    getter-setter test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+class Person {
+    public String name;
+}
+
+
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person();
 
+        person.name = "Julyun";
+
+        System.out.println(person.name);
     }
 }
